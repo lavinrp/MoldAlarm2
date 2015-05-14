024bdf246a827542d208ef9923a9be580dbb46df
lavinrp <lavinrp@gmail.com>
2015-05-14 09:10:29 -0400

    added ability to enlarge objects in the image. Enlarge refImage before subtraction.

--- CheckFrameForMold.m
+++ CheckFrameForMold.m
@@ -15,7 +15,7 @@ function moldFound = CheckFrameForMold(frameToCheck, referenceImage)
 %     imshow(cleanedImage);
 %     figure;
 
-    filteredImage = FilterImage4Step(frameToCheck, referenceImage);
+    filteredImage = FilterImage(frameToCheck, referenceImage);
     
     %count the remaining objects in view
     objCount = CountObjects(filteredImage);
@@ -23,19 +23,27 @@ function moldFound = CheckFrameForMold(frameToCheck, referenceImage)
     moldFound = false;
 end
 
-function filteredImage = FilterImage4Step(frameToCheck, referenceImage)
+function filteredImage = FilterImage(frameToCheck, referenceImage)
     %edge detect and subtract
     reducedImage = FindSubtractedEdge(frameToCheck, referenceImage);
     figure;
     imshow(reducedImage);
+    title('subtracted')
+    pause(2);
     
     %remove small invalid objects from image
-    cleanedImage = EliminateInvalidObjects(reducedImage, 1000, 10);
+    cleanedImage = EliminateInvalidObjects(reducedImage, 1000, 5);
     
     imshow(cleanedImage);
-    %TODO: enlarge remaining structures
+    title('cleaned')
+    
+    %enlarge remaining structures
+    %enhancedImage = EnlargeObjects(cleanedImage, 3);
+    
+    %imshow(enhancedImage);
     
     %TODO: remove remaining invalid structures
+    
     
     %TODO: return cleaned image
     
@@ -43,8 +51,16 @@ function filteredImage = FilterImage4Step(frameToCheck, referenceImage)
     
 end
 
+
+function returnImage = EnlargeObjects(image, radius)
+    structureElement = strel('disk', radius, 0);
+    %structureElement = strel('square', 5); alternate dialation option
+    returnImage = imdilate(image, structureElement);
+end
+
 function objCount = CountObjects(image)
     objCount = 1;
+    %TODO: fill stub
 end
 
 function cleanedImage = EliminateInvalidObjects(image, maxObjSize, minObjSize)
@@ -69,12 +85,18 @@ function cleanedImage = EliminateInvalidObjects(image, maxObjSize, minObjSize)
 end
 
 function returnImage = FindSubtractedEdge(image,refImg)
+    %TODO: posabilities for better subtracted image
+    %TODO: enlarge structures in refImg?
+    %TODO: subtract out last image too?
  
     %find the edges of the image to subtract from
     im = edge(image);
     
     %find the edges of the reference image(image to subtract)
     rim = edge(refImg);
+    
+    %Enlarge all objects in reference image to get rid of more noise
+    rim = EnlargeObjects(rim, 2);
     
     %return the result of the subtraction
     returnImage = im - rim;    
